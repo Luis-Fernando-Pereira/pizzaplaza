a88a9a977fd4313b914352a328a2b36b1bc63632
Luis Fernando <pereiraluisfernando.01.98@gmail.com>
2026-03-06 13:22:08 -0300

    Adicionado microserviço de produto (ProductService)

---
+++ product-service/src/test/java/br/com/pizzaplaza/productservice/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package br.com.pizzaplaza.productservice;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
