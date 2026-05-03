6d9ab674623bcc8f6e3e0e4c62945dacb558ac06
Luis Fernando <pereiraluisfernando.01.98@gmail.com>
2026-05-03 18:24:14 -0300

    Adicionado campo de data criação na superClasse de entidades

--- libraries/entity/src/main/java/br/com/pizzaplaza/entity/fatherofall/OdinDto.java
+++ libraries/entity/src/main/java/br/com/pizzaplaza/entity/fatherofall/OdinDto.java
@@ -2,7 +2,10 @@ package br.com.pizzaplaza.entity.fatherofall;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class OdinDto {
     public String oid;
+    public Date createdAt;
 }
