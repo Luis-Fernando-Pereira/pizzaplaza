a1fa0ad2fc095c344b6b1c0724130b9d37a5ff7f
Luis Fernando <pereiraluisfernando.01.98@gmail.com>
2026-05-04 09:30:47 -0300

    Adicionado protótipo de método para salvar novas categorias

--- product-service/src/main/java/br/com/pizzaplaza/product/services/ProductService.java
+++ product-service/src/main/java/br/com/pizzaplaza/product/services/ProductService.java
@@ -1,7 +1,15 @@
 package br.com.pizzaplaza.product.services;
 
+import br.com.pizzaplaza.entity.Category;
+import br.com.pizzaplaza.entity.dtos.CategoryDto;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class ProductService {
+
+    public CategoryDto save(CategoryDto dto) {
+        Category category = new Category();
+
+        category.set
+    }
 }
