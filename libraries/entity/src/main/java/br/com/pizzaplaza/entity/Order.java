06404a3354c85a4ab3f70ec97283a4e2c78092d6
Luis Fernando <pereiraluisfernando.01.98@gmail.com>
2026-05-17 18:51:11 -0300

    Removido @Data da entidade pedido

--- libraries/entity/src/main/java/br/com/pizzaplaza/entity/Order.java
+++ libraries/entity/src/main/java/br/com/pizzaplaza/entity/Order.java
@@ -4,16 +4,16 @@ import br.com.pizzaplaza.entity.fatherofall.Odin;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
 
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
 @Entity
 @Table(name = "order")
 public class Order extends Odin {
 
+    @Getter
     @OneToMany(mappedBy = "order")
     private Set<Pizza> pizzaSet = new HashSet<>();
 }
