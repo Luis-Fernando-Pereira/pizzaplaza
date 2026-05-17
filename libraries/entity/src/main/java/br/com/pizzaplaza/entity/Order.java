66fc6766554d2899021a365b984c21a14bd30114
Luis Fernando <pereiraluisfernando.01.98@gmail.com>
2026-05-17 19:04:24 -0300

    Adicionado enum de status do pedido ao pedido

--- libraries/entity/src/main/java/br/com/pizzaplaza/entity/Order.java
+++ libraries/entity/src/main/java/br/com/pizzaplaza/entity/Order.java
@@ -1,10 +1,13 @@
 package br.com.pizzaplaza.entity;
 
+import br.com.pizzaplaza.entity.enums.OrderStatus;
 import br.com.pizzaplaza.entity.fatherofall.Odin;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -12,6 +15,11 @@ import java.util.Set;
 @Entity
 @Table(name = "order")
 public class Order extends Odin {
+
+    @Getter
+    @Setter
+    @Column(name = "status", length = 20, nullable = false)
+    private OrderStatus status = OrderStatus.RECEIVED;
 
     @Getter
     @OneToMany(mappedBy = "order")
