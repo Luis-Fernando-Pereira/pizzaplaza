8be1f6dfe9890c44d2cbd5daf1a60c333f2508a5
Luis Fernando <pereiraluisfernando.01.98@gmail.com>
2026-05-17 18:43:57 -0300

    Entidade de pedido movido para diretório raiz

--- libraries/entity/src/main/java/br/com/pizzaplaza/entity/Order.java
+++ libraries/entity/src/main/java/br/com/pizzaplaza/entity/Order.java
@@ -1,5 +1,6 @@
-package br.com.pizzaplaza.entity.fatherofall;
+package br.com.pizzaplaza.entity;
 
+import br.com.pizzaplaza.entity.fatherofall.Odin;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -7,6 +8,6 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "order")
-public class Order extends Odin{
+public class Order extends Odin {
 
 }
