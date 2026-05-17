8fd4572e3719af08d91b2027876f92f499005f56
Luis Fernando <pereiraluisfernando.01.98@gmail.com>
2026-05-17 18:50:46 -0300

    Removido @Data da entidade pizza

--- libraries/entity/src/main/java/br/com/pizzaplaza/entity/Pizza.java
+++ libraries/entity/src/main/java/br/com/pizzaplaza/entity/Pizza.java
@@ -3,24 +3,30 @@ package br.com.pizzaplaza.entity;
 import br.com.pizzaplaza.entity.enums.PizzaSize;
 import br.com.pizzaplaza.entity.fatherofall.Odin;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Data
 @Entity
 @Table(name = "pizza")
 public class Pizza extends Odin {
 
+    @Getter
+    @Setter
     @Column(name = "size", length = 10, nullable = false)
     @Enumerated(EnumType.STRING)
     private PizzaSize size;
 
+    @Getter
+    @Setter
     @ManyToOne
     @JoinColumn(name = "order_oid")
     private Order order;
 
+    @Getter
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "pizza", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<PizzaFlavor> flavors;
+    private List<PizzaFlavor> flavors = new ArrayList<>();
 
 }
